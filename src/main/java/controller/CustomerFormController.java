88a31020675f2f0247846cb0448cc8e5f35ac196
Buddhika <buddhikadreaca@gmail.com>
2023-11-04 16:27:25 +0530

    Employeedash Finished

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -1,4 +1,91 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerFormController {
+    @FXML
+    private JFXButton btnDeleteCustomer;
+
+    @FXML
+    private JFXButton btnSaveCustomer;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerContact;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerID;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerName;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerPets;
+
+    @FXML
+    private RadioButton rdbtnCustomer2ndNum;
+
+    @FXML
+    private TableView<?> tblCustomer;
+
+    @FXML
+    private TextField txtContact2nd;
+
+    @FXML
+    private TextField txtContactNo;
+
+    @FXML
+    private TextField txtCustomerFname;
+
+    @FXML
+    private TextField txtCustomerID;
+
+    @FXML
+    private TextField txtCustomerLname;
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerContactSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerLnameSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerSaveOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerSearchOnaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCustomerFnameSearch(ActionEvent event) {
+
+    }
 }
---
+++ src/main/java/model/CustomerModel.java
@@ -0,0 +1,8 @@
+package model;
+
+public class CustomerModel {
+    private String customerId;
+    private String customerName;
+    private String customerAddress;
+    private String cust
+}
